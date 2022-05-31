de0ac10a5777902bd3d739727cf724f3245f3a82
Carlos Corti <carli@Carloss-MacBook-Pro.local>
2022-05-31 16:02:25 +0200

    use data from form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,13 @@ export class AppComponent {
   colorsRadio = ['blue', 'red'];
   defaulfColorsRadioChecked = 'blue';
 
+  selectedUsernameValue = '';
+  selectedEmailValue = '';
+  selectedSecretValue = '';
+  selectedColorsRadioChecked = '';
+
+  submitted = false;
+
   onSuggestUserNameClick() {
     this.signUpForm.form.patchValue({
       userDataInputs: {
@@ -36,7 +43,26 @@ export class AppComponent {
     });
   }
 
+  setDefaultFormValues() {
+    this.signUpForm.setValue({
+      colorRadioButtons: {
+        color: this.defaulfColorsRadioChecked,
+      },
+      secret: this.defaulfSelectValue,
+      userDataInputs: {
+        email: this.defaulfEmailValue,
+        username: this.defaulfUsernameValue,
+      },
+    });
+  }
+
   onSubmit() {
+    this.selectedUsernameValue = this.signUpForm.value.userDataInputs.username;
+    this.selectedEmailValue = this.signUpForm.value.userDataInputs.email;
+    this.selectedSecretValue = this.signUpForm.value.secret;
+    this.selectedColorsRadioChecked =
+      this.signUpForm.value.colorRadioButtons.color;
+    this.submitted = true;
     console.log('form', this.signUpForm);
   }
 }
